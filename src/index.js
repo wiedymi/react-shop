a6003f70e756e7b866188c0c60c76e4d1f60f251
Uladzislau Yakauleu <u.yalauleu@gmail.com>
2019-07-30 16:05:49 +0300

    Fixed Service Worker Issue

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import * as OfflinePluginRuntime from 'offline-plugin/runtime';
 import '@/styles/base/index.scss';
 import App from '@/App';
-import * as serviceWorker from '@/serviceWorker';
 import shop from '@/redux/store';
 
 const store = createStore(shop, applyMiddleware(thunk));
@@ -20,4 +20,15 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register({ skipWaiting: true });
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./sw.js').then(reg => {
+      console.log('Service worker registered.');
+    });
+  });
+}
+
+OfflinePluginRuntime.install({
+  onUpdateReady: () => OfflinePluginRuntime.applyUpdate(),
+  onUpdated: () => (window.swUpdate = true)
+});
